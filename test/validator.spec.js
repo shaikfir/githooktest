7486738a3386aa0dca5457fbc9ca350d6003e512
shaik <shaik@wix.com>
2016-04-30 02:34:59 +0300

    adding

---
+++ test/validator.spec.js
@@ -0,0 +1,10 @@
+var validator = require('../githooks/commit-msg');
+
+
+describe("Git Hook thingy", function() {
+  describe("commit message validator", function() {
+    it("should work", function() {
+      expect(1).to.equal(1);
+    });
+  });
+});
